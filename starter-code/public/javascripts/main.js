13673089c866f5fdbe68f1ede93cc46d67f675a3
Duc NGUYEN <nguyentduc92@gmail.com>
2019-04-22 23:26:12 +0200

    login and fix like icon

--- starter-code/public/javascripts/main.js
+++ starter-code/public/javascripts/main.js
@@ -28,16 +28,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-function toggleLikeBtn(){
-  const checkBtn = document.querySelector('.recipe__love use').getAttribute("href")
-  const iconString = checkBtn.includes("outlined") ? 'icon-heart' : 'icon-heart-outlined';
-  document.querySelector('.recipe__love use').setAttribute('href', `../icons.svg#${iconString}`);
-  // icons.svg#icon-heart-outlined
-};
+//Button Like
+function toggleLikeBtn(event){
+  let elementTarget = event.target.closest(".header__likes");
+  let html = elementTarget.innerHTML
+  if(html.includes("icon-heart-outlined")) {
+    html = html.replace("icon-heart-outlined","icon-heart")
+    elementTarget.innerHTML = html
+  } else{
+    html = html.replace("icon-heart","icon-heart-outlined")
+    elementTarget.innerHTML= html
+  }
+}
 
 
 const btnLike = document.querySelectorAll(".recipe__love")
 for(let i = 0; i< btnLike.length;i++){
   btnLike[i].onclick = toggleLikeBtn
 }
+
+
+//Login Modal On/Off
+
+function toggleLoginForm(){
+ const formLogin = document.querySelector('.modal')
+ formLogin.classList.contains("is-active") ? formLogin.classList.remove("is-active"):formLogin.classList.add("is-active")
+};
+
+
+const btnLogin = document.querySelectorAll('.login')
+const btnCloseModal = document.querySelector('.modal-close ')
+const backgroundCloseModal = document.querySelector('.modal-background')
+btnCloseModal.onclick = backgroundCloseModal.onclick= toggleLoginForm
+for(let i = 0; i< btnLogin.length;i++){
+  btnLogin[i].onclick = toggleLoginForm
+}
+
+
