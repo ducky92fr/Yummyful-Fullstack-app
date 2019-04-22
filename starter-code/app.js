dacaf55305445519bed4a22e2ac7458b902ec55b
Susan Almassi <susan.z.almassi@gmail.com>
2019-04-22 12:56:08 +0200

    second commit : nav bar working but problem with search box

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,16 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const express      = require('express');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
+const bodyParser = require("body-parser");
+const express = require("express");
+const hbs = require("hbs");
+const hbsutils = require("hbs-utils")(hbs);
+const mongoose = require("mongoose");
 const app = express();
 
-
-const homePageRoutes = require('./routes/homepage')
-
-
-
+const homePageRoutes = require("./routes/homepage");
 
 // Middleware Setup
 app.use(bodyParser.json());
@@ -18,24 +15,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Express View engine setup
 app.use(bodyParser.json()); //to parse json
-app.use(bodyParser.urlencoded({extended:false})); // to parse the regular form data
+app.use(bodyParser.urlencoded({ extended: false })); // to parse the regular form data
 app.use(express.static(__dirname + "/public"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
-
-
-
+hbsutils.registerPartials(__dirname + "/views/partials");
+hbsutils.registerWatchedPartials(__dirname + "/views/partials");
 
 app.use(homePageRoutes);
 
-
 mongoose
   .connect(
-    'mongodb+srv://ducky:ducbeo92@cluster0-owbdv.mongodb.net/shop?retryWrites=true',{ useNewUrlParser: true }
+    "mongodb+srv://ducky:ducbeo92@cluster0-owbdv.mongodb.net/shop?retryWrites=true",
+    { useNewUrlParser: true }
   )
   .then(() => {
-    console.log("succes")
+    console.log("success");
     app.listen(3000);
   })
   .catch(err => {
