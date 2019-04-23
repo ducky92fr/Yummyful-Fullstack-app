56035d50ed5f7307939392c80c8eeeb3651ea6be
Susan Almassi <susan.z.almassi@gmail.com>
2019-04-23 19:50:33 +0200

    search bar working 2

--- starter-code/public/javascripts/search-recipes.js
+++ starter-code/public/javascripts/search-recipes.js
@@ -3,28 +3,33 @@ const formSearch = document.getElementById("form-search");
 const image = document.querySelectorAll(".image-grid");
 const name = document.querySelectorAll(".recipe-name");
 const type = document.querySelectorAll(".type-dish");
+const searchInput = document.getElementById("input-search");
 const url = "http://localhost:3000/api";
 
+function displayResults(data) {
+  console.log("iciiiii");
+  let arrayLength;
+  data.length <= 9 ? (arrayLength = data.length) : (arrayLength = 9);
+  for (let i = 0; i < arrayLength; i++) {
+    image[i].src = data[i].imageURL;
+    name[i].innerText = data[i].title;
+    type[i].textContent = data[i].type;
+  }
+}
 function searchRecipes(e) {
   e.preventDefault();
-  const searchInput = document.getElementById("input-search").value;
-  console.log(searchInput);
   axios
-    .get(`${url}/search?q=${searchInput}`)
+    .get(`${url}/search?q=${searchInput.value}`)
     .then(result => {
-      console.log(result);
-      // result nous donne une nodelist. Pour chaque élément de la nodelist, il y a une data avec l'objet Recipe dedans et ses éléments. Donc pour simplifier, on crée une var "arrayData" qui sera un array d'objets Recipe
-      const arrayData = result.data;
-      console.log(arrayData);
-      console.log(image);
-      console.log(name);
-      console.log(type);
-      for (let i = 0; i < arrayData.length; i++) {
-        image[i].src = arrayData[i].imageURL;
-        name[i].innerText = arrayData[i].title;
-        type[i].innerText = arrayData[i].type;
-      }
+      displayResults(result.data);
     })
-    .catch(error => console.log("error"));
+    .catch(error => {
+      console.log("error");
+    });
 }
-searchBtn.onclick = formSearch.onsubmit = searchRecipes;
+
+formSearch.onsubmit = searchRecipes;
+
+// function yHandler() {
+//   const wrap = document.getElementById;
+// }
